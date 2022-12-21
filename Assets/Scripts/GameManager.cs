b13fa02b5148e6fbc026c7ad3ee5753a2bcec736
faraz vaissi <farazmrx@gmail.com>
2022-12-21 11:32:21 +0330

    Add scoring UI and cleanup code

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,20 +1,35 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public Ball ball;
+    public Paddle PlayerPaddle;
+    public Paddle ComputerPaddle;
+    public Text PlayerScoreText;
+    public Text ComputerscoreText;
+
     private int _playerScore;
     private int _computerScore;
     
     public void PlayerScores()
     {
         _playerScore++;
-        this.ball.ResetPosition();
+        this.PlayerScoreText.text = _playerScore.ToString();
+        ResetRound();
     }
 
     public void Computerscores()
     {
         _computerScore++;
+        this.ComputerscoreText.text = _computerScore.ToString();
+        ResetRound();
+    }
+
+    private void ResetRound() {
+        this.PlayerPaddle.ResetPosition();
+        this.ComputerPaddle.ResetPosition();
         this.ball.ResetPosition();
+        this.ball.AddStartingForce();
     }
 }
